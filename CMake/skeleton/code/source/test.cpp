272f469c3ea345132798cbe2876ba1cf4894f929
Adrian Buzatu <adrian.buzatu@cern.ch>
2019-05-29 14:45:12 +0200

    Add examples of C++ 14 and 17

--- CMake/skeleton/code/source/test.cpp
+++ CMake/skeleton/code/source/test.cpp
@@ -1,6 +1,24 @@
 #include "test.h"
 
+std::tuple<char, int, bool> mytuple()
+{
+    char a = 'a';
+    int i = 123;
+    bool b = true;
+    return std::make_tuple(a, i, b);
+}
+
 int main() {
   std::cout<<"Test: hello world!"<<std::endl;
+  auto var=5;
+  std::cout<<"var="<<var<<std::endl;
+  int my_array[5] = {1, 2, 3, 4, 5};
+  for (const auto& x : my_array) {
+    std::cout<<"x*2="<<x*2<<std::endl;
+  }
+  //auto integer_literal = 1'000'000; // works only in C++14
+  //auto [a, i, b] = mytuple(); // works only in C++17
+  //
+  //
   return 0;
 }

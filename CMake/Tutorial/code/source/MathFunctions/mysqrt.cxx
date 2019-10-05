b8153b28340264acdfac8e2037b6e59e0bf62b63
abuzatu <adrian.buzatu@cern.ch>
2019-10-05 19:52:58 +0200

    CMake Tutorial: add system introspection, if a function is defined

--- CMake/Tutorial/code/source/MathFunctions/mysqrt.cxx
+++ CMake/Tutorial/code/source/MathFunctions/mysqrt.cxx
@@ -1,6 +1,18 @@
+#include <stdio.h>
 #include <math.h>
+// to make available in the code variables that are taken originally from the CMakeLists.txt
+#include "TutorialConfig.h"
 
 double mysqrt(double inputValue) {
-  if (inputValue<0) return 0.0;
-  else return sqrt(inputValue);
+  if (inputValue<0) {
+    return 0.0;
+  } else {
+#if defined (HAVE_EXP) && defined (HAVE_LOG)
+    printf("We have exp and log, so we use return exp(log(inputValue)*0.5);\n");
+    return exp(log(inputValue)*0.5);
+#else
+    printf("We don't have exp and log, so we use return sqrt(inputValue);\n");
+    return sqrt(inputValue);
+#endif
+  }
 }

794d989ec2b2745b81fd2f5aa2f4d5adbaddd1a5
abuzatu <adrian.buzatu@cern.ch>
2019-10-05 18:38:34 +0200

    CMake Tutorial: add folder testInstallationFromTutorial

---
+++ CMake/testInstallationFromTutorial/code/source/test.cpp
@@ -0,0 +1,29 @@
+#include "test.h"
+#include "MathFunctions.h"
+
+std::tuple<char, int, bool> mytuple()
+{
+    char a = 'a';
+    int i = 123;
+    bool b = true;
+    return std::make_tuple(a, i, b);
+}
+
+int main() {
+  std::cout<<"Test: hello world!"<<std::endl;
+  auto var=5;
+  std::cout<<"var="<<var<<std::endl;
+  int my_array[5] = {1, 2, 3, 4, 5};
+  for (const auto& x : my_array) {
+    std::cout<<"x*2="<<x*2<<std::endl;
+  }
+
+  std::cout<<" "<<std::endl;
+  std::cout<<"With our own math library sqrt(3)="<<mysqrt(3)<<std::endl;
+
+  //auto integer_literal = 1'000'000; // works only in C++14
+  //auto [a, i, b] = mytuple(); // works only in C++17
+  //
+  //
+  return 0;
+}

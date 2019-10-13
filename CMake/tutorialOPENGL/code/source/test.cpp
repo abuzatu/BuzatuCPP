f8a572fefc82b2de0c201fc22f636894e052a3af
abuzatu <adrian.buzatu@cern.ch>
2019-10-13 17:56:49 +0200

    Add grid

--- CMake/tutorialOPENGL/code/source/test.cpp
+++ CMake/tutorialOPENGL/code/source/test.cpp
@@ -8,12 +8,52 @@ void theCube() {
   glutSolidCube(0.4);
 }
 
+// function that draws the grid
+void drawGrid() {
+  // draw one line 40 times
+  // the first 20 times, translated in the z axis, towards viewer, out of screen
+  // each line horizontal going left to right)
+  // the next 20 times, rotate with 270 degrees around the y vertical axis
+  // translate towards the right
+  // each line horizontal going from far to close to the reader
+  
+  // so a for loop 40 times
+  for (int i=0; i!=40; i++) {
+    glPushMatrix();
+    if (i<20) {
+      // translate along the z axis, by unit of i
+      glTranslatef(0,0,i); 
+    } else {
+      // first translate by a unit i along the x axis
+      glTranslatef(i-20,0,0);
+      // then rotate by 270 degrees (or -90 degrees) along the y axis
+      glRotatef(-90,0,1,0);
+
+    }
+    
+    glBegin(GL_LINES);
+    glColor3f(1,1,1);
+    glLineWidth(1);
+    glVertex3f(0,-0.1,0);
+    glVertex3f(19,-0.1,0);
+    glEnd();
+    glPopMatrix();
+  }
+  
+}
+
 // function that shows what is displayed at one time
 void display() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glLoadIdentity();
+  // translate to left and closer to us
+  glTranslatef(-13,+0,-45);
   // rotate the perspective by 40 degrees 
   // around the direction defined by vector from (0,0,0) to (1,1,0)
   glRotatef(40,1,1,0);
+  drawGrid();
   theCube();
+
   glutSwapBuffers();
 }
 
@@ -35,8 +75,7 @@ int main(int argc, char **argv) {
   glutInitWindowSize(800, 600);
   glutCreateWindow("");
   glutDisplayFunc(display);
-  glutMainLoop();
   init();
-
+  glutMainLoop();
   return 0;
 }

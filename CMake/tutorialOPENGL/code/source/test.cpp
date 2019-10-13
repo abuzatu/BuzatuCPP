a267b6c91d02141b42f113ea4fe3c5fd43f843f1
abuzatu <adrian.buzatu@cern.ch>
2019-10-13 17:00:02 +0200

    Clean and add a 3D cube

--- CMake/tutorialOPENGL/code/source/test.cpp
+++ CMake/tutorialOPENGL/code/source/test.cpp
@@ -1,27 +1,22 @@
 #include "test.h"
-#include "GLUT/glut.h"
 
-constexpr unsigned int WIDTH = 640;
-constexpr unsigned int HEIGHT = 480;
-uint8_t data[HEIGHT * WIDTH * 3];
-std::mutex lock;
+void theCube() {
+  glColor3f(1,1,1);
+  glutSolidCube(0.4);
+}
 
 void display() {
-  std::lock_guard<std::mutex> frame_lock(lock);
-  glClearColor(0, 0, 0, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
-  glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, data);
+  theCube();
   glutSwapBuffers();
-  glutPostRedisplay();
 }
 
 int main(int argc, char **argv) {
   std::cout<<"Test: hello world with OPENGL and GLUT!"<<std::endl;
 
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-  glutInitWindowSize(WIDTH, HEIGHT);
-  glutCreateWindow("GLUT");
+  glutInitDisplayMode(GLUT_DOUBLE);
+  glutInitWindowSize(800, 600);
+  glutCreateWindow("");
   glutDisplayFunc(display);
   glutMainLoop();
 

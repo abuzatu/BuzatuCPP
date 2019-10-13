00473644901a0101a89d0b6e689fbba114d4aeee
abuzatu <adrian.buzatu@cern.ch>
2019-10-13 17:15:24 +0200

    rotate the cube

--- CMake/tutorialOPENGL/code/source/test.cpp
+++ CMake/tutorialOPENGL/code/source/test.cpp
@@ -1,14 +1,20 @@
 #include "test.h"
 
 void theCube() {
-  glColor3f(1,1,1);
+  // set the colors in order R,G,B as floats from 0.0 to 1.0
+  glColor3f(0.3,0.2,0.7);
+  // set the size of the cube
   glutSolidCube(0.4);
 }
 
 void display() {
+  // rotate the perspective by 40 degrees 
+  // around the direction defined by vector from (0,0,0) to (1,1,0)
+  glRotatef(40,1,1,0);
   theCube();
   glutSwapBuffers();
 }
+
 
 int main(int argc, char **argv) {
   std::cout<<"Test: hello world with OPENGL and GLUT!"<<std::endl;
